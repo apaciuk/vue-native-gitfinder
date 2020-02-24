b76f6a99f85c2de2a435d8c2eacc21696de07855
Paul Anthony McGowan <xhostcom@gmail.com>
2020-02-24 21:53:56 +0000

    First Commit

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="statusbar"></view>   
+</template>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.statusbar {
+background-color: aqua;
+height: 20px;
+}
+</style>
+
+<view class="statusbar"></view>   
+</template>
